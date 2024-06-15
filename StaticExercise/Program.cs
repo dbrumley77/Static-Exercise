2000aac8509549bcca036bc6ac647a9591011331
Douglas <douglas-brumley@hotmail.com>
2024-06-15 09:36:24 -0400

    Excercise Completed

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace StaticExercise
+﻿using System.Security.Principal;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Call a static method
+            //TempConverter.FahrenheitToCelsius(20.5);
+            //ClassName.MethodName();
+
+            //needed for print to console
+            double answer1 = TempConverter.FahrenheitToCelsius(7.5);
+            Console.WriteLine(Math.Round(answer1, 2));
+
+
+            double answer2 = TempConverter.CelsiusToFahrenheit(-13.61111);
+            Console.WriteLine($"-13.61111 degree celsius is {answer2} degress fahrenheit.");
+
+           
+            
+
+
         }
+
+
+
+
     }
+
+
+
+
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+       
+
+
+
+
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            double celsius = (fTemp - 32) * 5 / 9;
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            double fahrenheit = (cTemp * 9) / 5 + 32;
+            return fahrenheit;
+        }
+
+
+    }
+
+
+
+}
